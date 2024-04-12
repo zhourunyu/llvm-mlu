85b1805d77bdf7e095eda7da08af380ab7e50e3e
zhourunyu <zhourunyu22z@ict.ac.cn>
2024-04-12 03:48:43 +0000

    Refactor samples
    Fix `isinf` and `isnan` ops

--- samples/libdevice/isfinite.cpp
+++ samples/libdevice/isfinite.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <math.h>
 
-#define UNARY_OP sycl::cbrt
+#define UNARY_OP sycl::isfinite
 #include "unary_op.h"
 
 constexpr int N = 1024;
@@ -14,9 +14,19 @@ int main() {
     sycl::queue q(selector);
     std::cout << "Running on device: "<< q.get_device().get_info<sycl::info::device::name>() << std::endl;
 
-    std::vector<float> a(N), b(N), b_host(N), d(N);
+    std::vector<float> a(N), d(N);
+    std::vector<int> b(N), b_host(N);
 
     initArray(a);
+    for (auto &i : a) {
+        auto p = rand() / (float)RAND_MAX;
+        if (p > 0.75)
+            i = INFINITY;
+        else if (p > 0.5)
+            i = -INFINITY;
+        else if (p > 0.25)
+            i = NAN;
+    }
     auto startTime = getTime();
 
     UnaryOp(q, a, b, d);
@@ -24,12 +34,12 @@ int main() {
     std::cout << "Time: " << endTime - startTime << "us" << std::endl;
 
     for (int i = 0; i < N; i++) {
-        b_host[i] = std::cbrt(a[i]);
+        b_host[i] = std::isfinite(a[i]);
     }
     int ret = compareResult(b, b_host);
 
     if (ret) {
-        std::cout << "Test failed for cbrt()." << std::endl;
+        std::cout << "Test failed for isfinite()." << std::endl;
         return ret;
     }
     std::cout << "Test passed." << std::endl;
---
+++ samples/unary_op.h
@@ -0,0 +1,29 @@
+#ifndef UNARY_OP
+#error "UNARY_OP not defined"
+#endif
+
+#include <CL/sycl.hpp>
+#include <vector>
+
+template <typename Ta, typename Tb>
+void UnaryOp(sycl::queue &q, const std::vector<Ta> &a, std::vector<Tb> &b, std::vector<Ta> &d) {
+    sycl::range<1> n_items{a.size()};
+
+    // bufferD在计算中没有用到，但是是必须存在的用于有效的寻址
+    sycl::buffer d_buffer(d);
+    sycl::buffer a_buffer(a);
+    sycl::buffer b_buffer(b);
+
+    q.submit([&](sycl::handler &h) {
+        sycl::accessor d_accessor(d_buffer, h, sycl::read_only);
+        sycl::accessor a_accessor(a_buffer, h, sycl::read_only);
+        sycl::accessor b_accessor(b_buffer, h, sycl::write_only, sycl::no_init);
+
+        h.parallel_for<class mm>(n_items, [=](sycl::id<1> i) {
+            b_accessor[i] = UNARY_OP(a_accessor[i]);
+        });
+    });
+    q.wait();
+}
+
+#undef UNARY_OP
