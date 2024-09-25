acd616a058365ae30c997ceea4313c0346f12d4c
zhourunyu <zhourunyu22z@ict.ac.cn>
2024-09-25 09:57:09 +0000

    Update README.md and env-cnrt.sh

--- README.md
+++ README.md
@@ -4,16 +4,16 @@ This project is based on llvm-13 for supporting cambricon mlu
 ## 使用方法
 
 ### 测试环境
-- Ubuntu 20.04，内核版本5.4.0-174-generic
+- Ubuntu 20.04.3 LTS，内核版本5.4.0-189-generic
 - MLU370-x8，驱动版本5.10.22
-- CNToolkit 3.6.1
-- GCC 9.4.0
+- CNToolkit 3.8.4
+- GCC 8.4.0
 
 ### 编译流程
 
 1. 下载代码及依赖
 ```shell
-git clone http://git.xcoresigma.com/open-source/llvm-mlu.git -b develop --recurse-submodules
+git clone http://git.xcoresigma.com/open-source/llvm-mlu.git --recurse-submodules
 ```
 2. 设置环境变量
 ```shell
@@ -24,27 +24,27 @@ source env-cnrt.sh
 3. 编译llvm
 ```shell
 python3 buildbot/configure.py -t Release --bang
-python3 buildbot/compile.py -j$(nproc)
+python3 buildbot/compile.py
 ```
 编译成功后，可以使用`sycl-ls`确认sycl是否正确找到MLU设备
 ```shell
 $ sycl-ls
-[cnrt:0] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.6.0]
-[cnrt:1] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.6.0]
-[cnrt:2] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.6.0]
-[cnrt:3] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.6.0]
-[cnrt:4] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.6.0]
-[cnrt:5] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.6.0]
-[cnrt:6] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.6.0]
-[cnrt:7] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.6.0]
-[cnrt:8] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.6.0]
-[cnrt:9] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.6.0]
-[cnrt:10] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.6.0]
-[cnrt:11] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.6.0]
-[cnrt:12] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.6.0]
-[cnrt:13] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.6.0]
-[cnrt:14] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.6.0]
-[cnrt:15] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.6.0]
+[ext_oneapi_cnrt:0] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.8.0]
+[ext_oneapi_cnrt:1] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.8.0]
+[ext_oneapi_cnrt:2] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.8.0]
+[ext_oneapi_cnrt:3] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.8.0]
+[ext_oneapi_cnrt:4] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.8.0]
+[ext_oneapi_cnrt:5] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.8.0]
+[ext_oneapi_cnrt:6] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.8.0]
+[ext_oneapi_cnrt:7] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.8.0]
+[ext_oneapi_cnrt:8] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.8.0]
+[ext_oneapi_cnrt:9] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.8.0]
+[ext_oneapi_cnrt:10] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.8.0]
+[ext_oneapi_cnrt:11] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.8.0]
+[ext_oneapi_cnrt:12] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.8.0]
+[ext_oneapi_cnrt:13] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.8.0]
+[ext_oneapi_cnrt:14] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.8.0]
+[ext_oneapi_cnrt:15] GPU : Cambricon BANG BACKEND 0.0 [CNRT 6.8.0]
 ```
 
 4. 编译libclc
@@ -59,7 +59,7 @@ bash libclc/build_mlisa.sh
 cd samples
 mkdir build && cd build
 cmake ..
-make -j$(nproc)
+make -j
 ./hello-sycl
 make test
 ```
--- env-cnrt.sh
+++ env-cnrt.sh
@@ -1,10 +1,101 @@
 #!/bin/bash
 
-SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+# Get absolute path to script. Gets a relative path as argument and outputs an absolute path.
+get_script_path() (
+  script_path="$1"
+  while [ -L "$script_path" ] ; do
+    script_dir=$(command dirname -- "$script_path")
+    script_dir=$(cd "$script_dir" && command pwd -P)
+    script_path="$(readlink "$script_path")"
+    case $script_path in
+      (/*) ;;
+       (*) script_path="$script_dir/$script_path" ;;
+    esac
+  done
+  script_dir=$(command dirname -- "$script_path")
+  script_dir=$(cd "$script_dir" && command pwd -P)
+  printf "%s" "$script_dir"
+)
+
+_vars_get_proc_name() {
+  if [ -n "${ZSH_VERSION:-}" ] ; then
+    script="$(ps -p "$$" -o comm=)"
+  else
+    script="$1"
+    while [ -L "$script" ] ; do
+      script="$(readlink "$script")"
+    done
+  fi
+  basename -- "$script"
+}
+
+_vars_this_script_name="env-cnrt.sh"
+if [ "$_vars_this_script_name" = "$(_vars_get_proc_name "$0")" ] ; then
+  echo ":: ERROR: Incorrect usage: this script must be sourced."
+  echo "   Usage: . path/to/${_vars_this_script_name}"
+  return 255 2>/dev/null || exit 255
+fi
+
+# Extract the name and location of this sourced script.
+
+# Generally, "ps -o comm=" is limited to a 15 character result, but it works
+# fine for this usage, because we are primarily interested in finding the name
+# of the execution shell, not the name of any calling script.
+
+vars_script_name=""
+vars_script_shell="$(ps -p "$$" -o comm=)"
+# ${var:-} needed to pass "set -eu" checks
+if [ -n "${ZSH_VERSION:-}" ] && [ -n "${ZSH_EVAL_CONTEXT:-}" ] ; then     # zsh 5.x and later
+  # shellcheck disable=2249
+  case $ZSH_EVAL_CONTEXT in (*:file*) vars_script_name="${(%):-%x}" ;; esac ;
+elif [ -n "${KSH_VERSION:-}" ] ; then                                     # ksh, mksh or lksh
+  if [ "$(set | grep -Fq "KSH_VERSION=.sh.version" ; echo $?)" -eq 0 ] ; then # ksh
+    vars_script_name="${.sh.file}" ;
+  else # mksh or lksh or [lm]ksh masquerading as ksh or sh
+    # force [lm]ksh to issue error msg; which contains this script's path/filename, e.g.:
+    # mksh: /workspace/llvm-mlu/env-cnrt.sh[137]: ${.sh.file}: bad substitution
+    vars_script_name="$( (echo "${.sh.file}") 2>&1 )" || : ;
+    vars_script_name="$(expr "${vars_script_name:-}" : '^.*sh: \(.*\)\[[0-9]*\]:')" ;
+  fi
+elif [ -n "${BASH_VERSION:-}" ] ; then        # bash
+  # shellcheck disable=2128
+  (return 0 2>/dev/null) && vars_script_name="${BASH_SOURCE}" ;
+elif [ "dash" = "$vars_script_shell" ] ; then # dash
+  # force dash to issue error msg; which contains this script's rel/path/filename, e.g.:
+  # dash: 146: /workspace/llvm-mlu/env-cnrt.sh: Bad substitution
+  vars_script_name="$( (echo "${.sh.file}") 2>&1 )" || : ;
+  vars_script_name="$(expr "${vars_script_name:-}" : '^.*dash: [0-9]*: \(.*\):')" ;
+elif [ "sh" = "$vars_script_shell" ] ; then   # could be dash masquerading as /bin/sh
+  # force a shell error msg; which should contain this script's path/filename
+  # sample error msg shown; assume this file is named "env-cnrt.sh"
+  vars_script_name="$( (echo "${.sh.file}") 2>&1 )" || : ;
+  if [ "$(printf "%s" "$vars_script_name" | grep -Eq "sh: [0-9]+: .*env-cnrt\.sh: " ; echo $?)" -eq 0 ] ; then # dash as sh
+    # sh: 155: /workspace/llvm-mlu/env-cnrt.sh: Bad substitution
+    vars_script_name="$(expr "${vars_script_name:-}" : '^.*sh: [0-9]*: \(.*\):')" ;
+  fi
+else  # unrecognized shell or dash being sourced from within a user's script
+  # force a shell error msg; which should contain this script's path/filename
+  # sample error msg shown; assume this file is named "env-cnrt.sh"
+  vars_script_name="$( (echo "${.sh.file}") 2>&1 )" || : ;
+  if [ "$(printf "%s" "$vars_script_name" | grep -Eq "^.+: [0-9]+: .*env-cnrt\.sh: " ; echo $?)" -eq 0 ] ; then # dash
+    vars_script_name="$(expr "${vars_script_name:-}" : '^.*: [0-9]*: \(.*\):')" ;
+  else
+    vars_script_name="" ;
+  fi
+fi
+
+if [ "" = "$vars_script_name" ] ; then
+  >&2 echo ":: ERROR: Unable to proceed: possible causes listed below."
+  >&2 echo "   This script must be sourced. Did you execute or source this script?" ;
+  >&2 echo "   Unrecognized/unsupported shell (supported: bash, zsh, ksh, m/lksh, dash)." ;
+  >&2 echo "   Can be caused by sourcing from ZSH version 4.x or older." ;
+  return 255 2>/dev/null || exit 255
+fi
+
+DPCPP_ROOT=$(get_script_path "${vars_script_name:-}")
 export SYCL_DEVICE_FILTER=cnrt
-export DPCPP_HOME=$SCRIPT_PATH
-export PATH=$DPCPP_HOME/build/bin:$PATH
-export LD_LIBRARY_PATH=$DPCPP_HOME/build/lib:$LD_LIBRARY_PATH
+export PATH=$DPCPP_ROOT/build/bin:$PATH
+export LD_LIBRARY_PATH=$DPCPP_ROOT/build/lib:$LD_LIBRARY_PATH
 
 export NEUWARE_HOME=/usr/local/neuware
 export LD_LIBRARY_PATH=${NEUWARE_HOME}/lib64:${LD_LIBRARY_PATH}
