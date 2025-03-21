404472113ce688064f5faa3c8615289f069d96d1
zhourunyu <zhourunyu22z@ict.ac.cn>
2025-03-21 06:14:10 +0000

    optimize vector_max with relu

--- clang/lib/CodeGen/CGBuiltin.cpp
+++ clang/lib/CodeGen/CGBuiltin.cpp
@@ -16011,6 +16011,51 @@ Value *CodeGenFunction::EmitMLISABuiltinExpr(unsigned BuiltinID,
     Value *Src2 = EmitScalarExpr(E->getArg(2));
     return Builder.CreateCall(F, {Src0, Src1, Src2});
   }
+  case MLISA::BI__mlvm_stream_relu_f16: {
+    llvm::Function *F = CGM.getIntrinsic(Intrinsic::mlvm_stream_relu_f16);
+    Value *Src0x = EmitScalarExpr(E->getArg(0));
+    Value *Src0 = Builder.CreatePointerBitCastOrAddrSpaceCast(Src0x, Int8PtrTy);
+    Value *Src1x = EmitScalarExpr(E->getArg(1));
+    Value *Src1 = Builder.CreatePointerBitCastOrAddrSpaceCast(Src1x, Int8PtrTy);
+    Value *Src2 = EmitScalarExpr(E->getArg(2));
+    return Builder.CreateCall(F, {Src0, Src1, Src2});
+  }
+  case MLISA::BI__mlvm_stream_relu_f32: {
+    llvm::Function *F = CGM.getIntrinsic(Intrinsic::mlvm_stream_relu_f32);
+    Value *Src0x = EmitScalarExpr(E->getArg(0));
+    Value *Src0 = Builder.CreatePointerBitCastOrAddrSpaceCast(Src0x, Int8PtrTy);
+    Value *Src1x = EmitScalarExpr(E->getArg(1));
+    Value *Src1 = Builder.CreatePointerBitCastOrAddrSpaceCast(Src1x, Int8PtrTy);
+    Value *Src2 = EmitScalarExpr(E->getArg(2));
+    return Builder.CreateCall(F, {Src0, Src1, Src2});
+  }
+  case MLISA::BI__mlvm_stream_relu_s8: {
+    llvm::Function *F = CGM.getIntrinsic(Intrinsic::mlvm_stream_relu_s8);
+    Value *Src0x = EmitScalarExpr(E->getArg(0));
+    Value *Src0 = Builder.CreatePointerBitCastOrAddrSpaceCast(Src0x, Int8PtrTy);
+    Value *Src1x = EmitScalarExpr(E->getArg(1));
+    Value *Src1 = Builder.CreatePointerBitCastOrAddrSpaceCast(Src1x, Int8PtrTy);
+    Value *Src2 = EmitScalarExpr(E->getArg(2));
+    return Builder.CreateCall(F, {Src0, Src1, Src2});
+  }
+  case MLISA::BI__mlvm_stream_relu_s16: {
+    llvm::Function *F = CGM.getIntrinsic(Intrinsic::mlvm_stream_relu_s16);
+    Value *Src0x = EmitScalarExpr(E->getArg(0));
+    Value *Src0 = Builder.CreatePointerBitCastOrAddrSpaceCast(Src0x, Int8PtrTy);
+    Value *Src1x = EmitScalarExpr(E->getArg(1));
+    Value *Src1 = Builder.CreatePointerBitCastOrAddrSpaceCast(Src1x, Int8PtrTy);
+    Value *Src2 = EmitScalarExpr(E->getArg(2));
+    return Builder.CreateCall(F, {Src0, Src1, Src2});
+  }
+  case MLISA::BI__mlvm_stream_relu_s32: {
+    llvm::Function *F = CGM.getIntrinsic(Intrinsic::mlvm_stream_relu_s32);
+    Value *Src0x = EmitScalarExpr(E->getArg(0));
+    Value *Src0 = Builder.CreatePointerBitCastOrAddrSpaceCast(Src0x, Int8PtrTy);
+    Value *Src1x = EmitScalarExpr(E->getArg(1));
+    Value *Src1 = Builder.CreatePointerBitCastOrAddrSpaceCast(Src1x, Int8PtrTy);
+    Value *Src2 = EmitScalarExpr(E->getArg(2));
+    return Builder.CreateCall(F, {Src0, Src1, Src2});
+  }
   case MLISA::BI__mlvm_stream_active_abs_f32: {
     llvm::Function *F = CGM.getIntrinsic(Intrinsic::mlvm_stream_active_abs_f32);
     Value *Src0x = EmitScalarExpr(E->getArg(0));
--- libclc/mlisa/libspirv/vector/vector_max.cl
+++ libclc/mlisa/libspirv/vector/vector_max.cl
@@ -11,6 +11,27 @@
 #include "../../include/libdevice.h"
 #include <clcmacro.h>
 
+// use relu when y == 0
+#define VECTOR_MAX_SCALAR_RELU(TYPE, NAME)                                \
+void __vector_max_scalar_##NAME(int n, TYPE *z, const TYPE *x, TYPE y) {  \
+  if (y == (TYPE)0)                                                       \
+    __mlvm_stream_relu_##NAME(z, (TYPE *)x, n);                           \
+  else                                                                    \
+    __cn_vector_max_scalar_##NAME(n, z, x, y);                            \
+}
+#define VECTOR_MAX_SCALAR(TYPE, NAME)                                     \
+void __vector_max_scalar_##NAME(int n, TYPE *z, const TYPE *x, TYPE y) {  \
+  __cn_vector_max_scalar_##NAME(n, z, x, y);                              \
+}
+VECTOR_MAX_SCALAR_RELU(float, f32)
+VECTOR_MAX_SCALAR_RELU(half, f16)
+VECTOR_MAX_SCALAR_RELU(int, s32)
+VECTOR_MAX_SCALAR(unsigned int, u32)
+VECTOR_MAX_SCALAR(long, s64)
+VECTOR_MAX_SCALAR(unsigned long, u64)
+#undef VECTOR_MAX_SCALAR_RELU
+#undef VECTOR_MAX_SCALAR
+
 #define __CLC_FUNCTION __spirv_ocl_vector_max
 #define __CLC_BUILTIN __cn_vector_max
 #define __CLC_BUILTIN_F __CLC_XCONCAT(__CLC_BUILTIN, _f32)
@@ -19,7 +40,7 @@
 #define __CLC_BUILTIN_U __CLC_XCONCAT(__CLC_BUILTIN, _u32)
 #define __CLC_BUILTIN_L __CLC_XCONCAT(__CLC_BUILTIN, _s64)
 #define __CLC_BUILTIN_UL __CLC_XCONCAT(__CLC_BUILTIN, _u64)
-#define __CLC_BUILTIN_SCALAR __cn_vector_max_scalar
+#define __CLC_BUILTIN_SCALAR __vector_max_scalar
 #define __CLC_BUILTIN_SCALAR_F __CLC_XCONCAT(__CLC_BUILTIN_SCALAR, _f32)
 #define __CLC_BUILTIN_SCALAR_H __CLC_XCONCAT(__CLC_BUILTIN_SCALAR, _f16)
 #define __CLC_BUILTIN_SCALAR_I __CLC_XCONCAT(__CLC_BUILTIN_SCALAR, _s32)
